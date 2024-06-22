7d7774266e4a417a6b5c71ec8492557431cf2d33
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2024-06-22 17:29:46 -0400

    Add firewall rules for USherbrooke

--- usr/share/system-scripts/update/main.sh
+++ usr/share/system-scripts/update/main.sh
@@ -3,4 +3,4 @@ eselect news read new
 emerge --ask --verbose --update --keep-going --backtrack=100 --deep --newuse --with-bdeps=y @world
 doas -u $DOAS_USER cp --force --reflink=auto /var/lib/portage/world $GENTOO_CONFIG_DIR/var/lib/portage/world
 doas -u $DOAS_USER cargo install-update -a
-npm update -g"
+npm update -g
