a840224be407b07d74852ee76295432014812a36
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2024-06-22 20:25:01 -0400

    Include PWA patch process in update-firefox

--- usr/share/system-scripts/update-firefox/main.sh
+++ usr/share/system-scripts/update-firefox/main.sh
@@ -2,6 +2,17 @@ doas -u $DOAS_USER killall firefox
 cp --force --reflink=auto /home/$DOAS_USER/Configuration/policies.json /usr/lib64/firefox/distribution/policies.json
 doas -u $DOAS_USER rm --recursive --force /home/$DOAS_USER/.local/share/firefoxpwa/runtime/*
 doas -u $DOAS_USER cp --recursive --force --reflink=always /usr/lib64/firefox/* /home/$DOAS_USER/.local/share/firefoxpwa/runtime
+if [ ! -d /tmp/firefoxpwa-src ]; then
+	doas -u $DOAS_USER rm -rf "/tmp/firefoxpwa-src"
+	doas -u $DOAS_USER mkdir -p "/tmp/firefoxpwa-src/native/userchrome"
+	cd "/tmp/firefoxpwa-src"
+	doas -u $DOAS_USER git init
+	doas -u $DOAS_USER git remote add origin -f "https://github.com/filips123/PWAsForFirefox.git"
+	doas -u $DOAS_USER git sparse-checkout set native/userchrome/
+	doas -u $DOAS_USER git pull origin main
+fi
+cd "/tmp/firefoxpwa-src"
+cp -rf "/tmp/firefoxpwa-src/native/userchrome/runtime/." "/usr/lib64/firefox"
 doas -u $DOAS_USER /usr/share/system-scripts/update-firefox/update-firefox-profile.sh .mozilla/firefox/tiuh6kpf.default $DOAS_USER
 doas -u $DOAS_USER /usr/share/system-scripts/update-firefox/update-firefox-profile.sh .mozilla/firefox/3v4f2jgx.Vanilla $DOAS_USER
 doas -u $DOAS_USER /usr/share/system-scripts/update-firefox/update-firefox-profile.sh .local/share/firefoxpwa/profiles/01HG8VMTPMBMRZQXJKKDEQH6KS $DOAS_USER
--- usr/share/system-scripts/update-firefox/update-firefox-profile.sh
+++ usr/share/system-scripts/update-firefox/update-firefox-profile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 FIREFOXPREFSPATH="/home/$2/Configuration/firefox-prefs/"
 FIREFOXPROFILEPATH="/home/$2/$1"
+FIREFOXPWASRCPATH="/tmp/firefoxpwa-src/native/userchrome"
 rm "$FIREFOXPROFILEPATH/prefs.js"
 rm "$FIREFOXPROFILEPATH/user.js"
+if [[ ""$1"" == *firefoxpwa* ]]; then
+	rm -rf "$FIREFOXPROFILEPATH/chrome/*"
+	cp -rf "$FIREFOXPWASRCPATH/profile/chrome/." "$FIREFOXPROFILEPATH/chrome"
+fi
 sed "s|.*|$FIREFOXPREFSPATH&|g" $FIREFOXPROFILEPATH/prefs.list | while read -r PREF; do cat $PREF >> "$FIREFOXPROFILEPATH/user.js"; done
