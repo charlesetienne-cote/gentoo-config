17c76e485924a2592cc1b412eed9254f096e51de
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2024-12-11 02:53:52 -0500

    Finish preliminary /init

--- initramfs.sh
+++ initramfs.sh
@@ -8,3 +8,14 @@ mount -nt tmpfs none /run
 
 # Rescue shell
 exec /bin/busybox sh
+
+mount -nt btrfs -o noacl,autodefrag,compress-force=zstd,datacow,datasum,disc
+ard=async,space_cache=v2,ssd_spread,noatime,rw,suid,dev,exec,async /dev/mapper/C
+ryptRoot /mnt/root
+
+umount /run
+umount /sys
+umount /proc
+umount /dev
+
+exec switch_root /mnt/root /sbin/init
